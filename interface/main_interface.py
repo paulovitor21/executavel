15864d48178f587cad3969e9e7e3d1d0287f57cd
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2024-06-02 11:44:58 -0400

    fix: modules

---
+++ interface/main_interface.py
@@ -0,0 +1,105 @@
+import customtkinter as ctk
+from tkinter.scrolledtext import ScrolledText
+from PIL import Image
+from customtkinter import CTkImage
+from modules.funtion import carregar_pdf, carregar_excel, processar, export_report, check_files
+
+def create_interface():
+    # configuração janela principal
+    app = ctk.CTk()
+    app.geometry("800x500")
+    app.title("Import PO")
+    ctk.set_appearance_mode("dark")
+
+    # frame esquerdo - barra lateral
+    frame_esquerdo = ctk.CTkFrame(app, width=500)
+    frame_esquerdo.grid(row=0, column=0, rowspan=2, sticky="nswe")
+
+    # ajuste de proporção das colunas e linhas do frame_esquerdo
+    frame_esquerdo.grid_rowconfigure(0, weight=1)
+    frame_esquerdo.grid_rowconfigure(1, weight=0)
+    frame_esquerdo.grid_rowconfigure(2, weight=0)
+    frame_esquerdo.grid_rowconfigure(3, weight=0)
+    frame_esquerdo.grid_columnconfigure(0, weight=1)
+
+    logo_image = Image.open("assets/logo.png")
+    logo_image = logo_image.resize((173, 45))
+    logo_ctk_image = CTkImage(light_image=logo_image, size=(173, 45))
+    logo_label = ctk.CTkLabel(frame_esquerdo, image=logo_ctk_image, text="")
+    logo_label.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="n")
+
+    # Centralização do texto do título e versão
+    titulo_label = ctk.CTkLabel(frame_esquerdo, text="Import PO", font=ctk.CTkFont(size=20, weight="bold"))
+    titulo_label.grid(row=1, column=0, padx=10, pady=(5, 10), sticky="n")
+
+    versao_label = ctk.CTkLabel(frame_esquerdo, text=("versão 1.0"))
+    versao_label.grid(row=2, column=0, padx=10, pady=(0, 10), sticky="s")
+
+    # frame direito - principal
+    frame_direito = ctk.CTkFrame(app)
+    frame_direito.grid(row=0, column=1, padx=10, pady=10, stick="nswe")
+
+    # ajuste das colunas - frame direito
+    frame_direito.grid_columnconfigure(0, weight=1)
+    frame_direito.grid_columnconfigure(1, weight=3)
+    frame_direito.grid_columnconfigure(2, weight=0)
+
+    # Texto - selecionar PDF
+    pdf_label = ctk.CTkLabel(frame_direito, text="PDF")
+    pdf_label.grid(row=0, column=0, padx=5, pady=5, stick="e")
+
+    # campo para inserir o pdf
+    pdf_entry = ctk.CTkEntry(frame_direito)
+    pdf_entry.grid(row=0, column=1, padx=5, pady=5, stick="we")
+
+    # Botão importar pdf
+    carregar_pdf_btn = ctk.CTkButton(frame_direito, text="Importar", command=lambda: carregar_pdf(pdf_entry, log_text, lambda: check_files(pdf_entry, excel_entry, process_btn)), fg_color="red", font=ctk.CTkFont(weight="bold"))
+    carregar_pdf_btn.grid(row=0, column=2, padx=5, pady=5)
+
+    # Texto - selecionar Excel
+    excel_label = ctk.CTkLabel(frame_direito, text="Excel")
+    excel_label.grid(row=1, column=0, padx=5, pady=5, sticky="e")
+
+    # campo para inserir o excel
+    excel_entry = ctk.CTkEntry(frame_direito)
+    excel_entry.grid(row=1, column=1, padx=5, pady=5, sticky="we")
+
+    # Botão importar excel
+    carregar_excel_btn = ctk.CTkButton(frame_direito, text="Importar", command=lambda: carregar_excel(excel_entry, log_text, lambda: check_files(pdf_entry, excel_entry, process_btn)), fg_color="red", font=ctk.CTkFont(weight="bold"))
+    carregar_excel_btn.grid(row=1, column=2, padx=5, pady=5)
+
+    frame_btn = ctk.CTkFrame(frame_direito, fg_color="transparent")
+    frame_btn.grid(row=2, column=0, columnspan=3, padx=5, pady=20)
+
+    process_btn = ctk.CTkButton(frame_btn, text="Processar", state=ctk.DISABLED, command=lambda: processar(log_text, export_btn), fg_color="red", font=ctk.CTkFont(weight="bold"))
+    process_btn.pack(side=ctk.LEFT, padx=10)
+
+    export_btn = ctk.CTkButton(frame_btn, text="Exportar Relatório", state=ctk.DISABLED, command=lambda: export_report(log_text, pdf_entry.get(), excel_entry.get()), fg_color="red", font=ctk.CTkFont(weight="bold"))
+    export_btn.pack(side=ctk.LEFT, padx=10)
+
+    # centralização dos botões processar e exportar
+    frame_direito.grid_columnconfigure(0, weight=1)
+    frame_direito.grid_rowconfigure(2, weight=1)
+
+    # frame logs de processamento
+    frame_logs = ctk.CTkFrame(app)
+    frame_logs.grid(row=1, column=1, padx=10, pady=10, sticky="nswe")
+
+    log_label = ctk.CTkLabel(frame_logs, text="Registro de Processamento")
+    log_label.pack(pady=5)
+
+    log_text = ScrolledText(frame_logs, height=10)
+    log_text.pack(expand=True, fill="both", padx=10, pady=10)
+    log_text.configure(state="disabled")
+    # config font do log
+    # Configurar a fonte da tag 'log'
+    log_text.tag_configure("log", font=("Helvetica", 12))
+
+    app.grid_columnconfigure(1, weight=1)
+    app.grid_rowconfigure(1, weight=1)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_interface()
+    app.mainloop()
---
+++ modules/funtion.py
@@ -0,0 +1,105 @@
+import customtkinter as ctk
+import datetime
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+from PIL import Image, ImageTk
+from customtkinter import CTkImage
+from docx import Document
+import fitz
+import pandas as pd
+
+
+data_hora_atual = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
+
+# função carregar arquivo
+def carregar_pdf(pdf_entry, log_text, check_files):
+    global pdf_path
+    pdf_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+    if pdf_path:
+        pdf_entry.delete(0, ctk.END)
+        pdf_entry.insert(0, pdf_path)
+        log_text.configure(state="normal")
+        log_text.insert(ctk.END, f"{data_hora_atual} - PDF importado com sucesso...\n", "log")
+        log_text.configure(state="disabled")
+        check_files()
+
+def carregar_excel(excel_entry, log_text, check_files):
+    global excel_path
+    excel_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+    if excel_path:
+        excel_entry.delete(0, ctk.END)
+        excel_entry.insert(0, excel_path)
+        log_text.configure(state="normal")
+        log_text.insert(ctk.END, f"{data_hora_atual} - Excel importado com sucesso...\n", "log")
+        log_text.configure(state="disabled")
+        check_files()
+
+def processar(log_text, export_btn):
+    log_text.configure(state="normal")
+    log_text.insert(ctk.END, f"{data_hora_atual} - Processamento iniciado...\n", "log")
+    log_text.configure(state="disabled")
+    log_text.configure(state="normal")
+    log_text.insert(ctk.END, f"{data_hora_atual} - Arquivos processados com sucesso!\n", "log")
+    log_text.configure(state="disabled")
+    export_btn.configure(state=ctk.NORMAL)
+
+def extrair_texto_pdf(pdf_path, texto_procurado):
+    documento = fitz.open(pdf_path)
+    for pagina in documento:
+        texto = pagina.get_text()
+        if texto_procurado in texto:
+            # encontrar posicao do texto
+            posicao_inicio = texto.find(texto_procurado)
+            if posicao_inicio != -1:
+                # extrair texto após import po
+                posicao_fim = texto.find(" ", posicao_inicio + len(texto_procurado) + 1)
+                texto_extraido = texto[posicao_inicio + len(texto_procurado) + 1:posicao_fim].strip()
+                return texto_extraido.split()[0]
+    return None
+
+# Função para extrair a descrição da planilha Excel
+def extrair_description(excel_path):
+    df = pd.read_excel(excel_path)
+    if 'description' in df.columns:
+        return df['description'].dropna().tolist()  # Retorna todas as descrições como uma lista
+    return None
+
+def criar_documento_word(texto, texto_excel, file_path):
+    doc = Document()
+    doc.add_heading("Relatório de Processamento", 0)
+    doc.add_paragraph('Este é um relatório de exemplo.')
+    doc.add_paragraph(f'Import PO No: {texto}')
+    if texto_excel:
+        for desc in texto_excel:
+            doc.add_paragraph(f'Description: {desc}')
+    doc.add_paragraph(f'Data e Hora de criação: {data_hora_atual}')
+    doc.save(file_path)
+
+
+def export_report(log_text, pdf_path, excel_path):
+    log_text.configure(state="normal")
+    log_text.insert(ctk.END, f"{data_hora_atual} - Exportação do relatório iniciada...\n", "log")
+    log_text.configure(state="disabled")
+
+    # abri a caixa de dialogo do windows
+    file_path = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word Files", "*.docx")])
+
+    if file_path:
+        texto = extrair_texto_pdf(pdf_path, "Import PO No")
+        texto_excel = extrair_description(excel_path)
+
+        if texto and texto_excel:
+            criar_documento_word(texto, texto_excel, file_path)
+            
+            log_text.configure(state="normal")
+            log_text.insert(ctk.END, f"{data_hora_atual} - Relatório exportado com sucesso!\n", "log")
+            log_text.configure(state="disabled")
+        else:
+            log_text.configure(state="normal")
+            log_text.insert(ctk.END, f"{data_hora_atual} - Erro ao Exportar Relatório!\n", "log")
+            log_text.configure(state="disabled")
+
+def check_files(pdf_entry, excel_entry, process_btn):
+    if pdf_entry.get() and excel_entry.get():
+        process_btn.configure(state=ctk.NORMAL)
+
